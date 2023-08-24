f8db0bc576633d0aa3273915f0b10056cb558fc3
Mayank <mayanksharmakv@gmail.com>
2023-08-24 12:16:11 +0530

    update server

--- index.js
+++ index.js
@@ -8,14 +8,16 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index");
 });
 
 app.post("/submit", (req, res) => {
   const randomAdj = adj[Math.floor(Math.random() * adj.length)];
   const randomNoun = noun[Math.floor(Math.random() * noun.length)];
-  res.render("index.ejs", { adjective: randomAdj, noun: randomNoun });
+  res.render("index", { adjective: randomAdj, noun: randomNoun });
 });
 
 app.listen(port, () => {
